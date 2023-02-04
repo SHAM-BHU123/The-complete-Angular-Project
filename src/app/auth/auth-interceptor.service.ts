9119e57fef01fa6bc31730066bc24933186c7cf3
shambhu pandey <sorajpandey666@gmail.com>
2023-02-04 23:00:41 +0545

    Working on interceptor

---
+++ src/app/auth/auth-interceptor.service.ts
@@ -0,0 +1,12 @@
+import { HttpInterceptor } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthInterceptorService implements HttpInterceptor {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(req);
+  }
+}
