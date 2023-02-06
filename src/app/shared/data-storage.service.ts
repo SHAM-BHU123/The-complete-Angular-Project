af9b7d90801ede7a1c1bd67bfeb7dbc9dc010929
shambhu pandey <sorajpandey666@gmail.com>
2023-02-06 16:18:24 +0545

    Attaching the Token with an interceptor

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
-import { exhaustMap, map, take, tap } from 'rxjs';
+import { exhaustMap, map, pipe, take, tap } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
 @Injectable({
   providedIn: 'root',
@@ -26,27 +26,20 @@ export class DataStorageService {
       });
   }
   fetchRecipes() {
-    return this.authService.user.pipe(
-      take(1),
-      exhaustMap((user) => {
-        return this.http.get<Recipe[]>(
-          'https://angular-project-aa83d-default-rtdb.firebaseio.com/recipes.json',
-          {
-            params: new HttpParams().set('auth', user.token),
-          }
-        );
-      }),
-      map((recipes) => {
-        return recipes.map((recipe) => {
-          return {
-            ...recipe,
-            ingredients: recipe.ingredients ? recipe.ingredients : [],
-          };
-        });
-      }),
-      tap((recipes) => {
-        this.recipeService.setRecipes(recipes);
-      })
-    );
+    return this.http
+      .get<Recipe[]>(
+        `https://angular-project-aa83d-default-rtdb.firebaseio.com/recipes.json`
+      )
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        }),
+        tap((recipes) => (this.recipeService.Recipes = recipes))
+      );
   }
 }
