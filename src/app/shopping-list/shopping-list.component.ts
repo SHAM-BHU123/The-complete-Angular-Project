b9c460598be90823302165225eb7e585cbcb63cf
shambhu pandey <sorajpandey666@gmail.com>
2023-02-09 20:07:28 +0545

    distroy the subscription

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -10,15 +10,17 @@ import { Subscription } from 'rxjs';
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[] = [];
-  private igChangeSub!: Subscription;
+  private igChangeSub: Subscription;
   constructor(private slService: ShoppingListServices) {}
 
   ngOnInit(): void {
     this.ingredients = this.slService.getIngredients();
     // this.ingredientChanged.emit(this.ingredients.slice());
-    this.slService.ingredientChanged.subscribe((ingredients: Ingredient[]) => {
-      this.ingredients = ingredients;
-    });
+    this.igChangeSub = this.slService.ingredientChanged.subscribe(
+      (ingredients: Ingredient[]) => {
+        this.ingredients = ingredients;
+      }
+    );
   }
   ngOnDestroy(): void {
     this.igChangeSub.unsubscribe();
